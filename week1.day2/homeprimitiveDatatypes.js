dd94f400ad88e328c698cedfbaaa5ae0a659196e
haripriya-0401 <haripriya.n0401@gmail.com>
2026-07-09 14:42:54 +0200

    Add files via upload

---
+++ week1.day2/homeprimitiveDatatypes.js
@@ -0,0 +1,26 @@
+let userName = 'Haripriya';
+console.log(userName);
+console.log(typeof userName);
+
+let organization =`TBG`;
+console.log(organization);
+console.log(typeof organization);
+
+let contactNumber = 256896325;
+console.log(contactNumber);
+console.log(typeof contactNumber);
+
+let knowsAutomation = true;
+console.log(knowsAutomation);
+console.log(typeof knowsAutomation);
+
+let usesPlaywright;
+console.log(usesPlaywright);
+console.log(typeof usesPlaywright);
+
+
+
+
+
+
+
