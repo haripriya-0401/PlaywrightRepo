69df2450da4e73948a24faf6c8d8d0bde597b3f3
haripriya-0401 <haripriya.n0401@gmail.com>
2026-07-13 14:14:17 +0200

    Add files via upload

---
+++ week1.day4/homesquaresinreverseorder.js
@@ -0,0 +1,9 @@
+function squares(){
+    let sq;
+for( i=10; i>=1; i--){     // for loop iterating from 10 to 1
+    sq = i*i               // squares of numbers
+    console.log("Squares in reverse order" ,sq)
+}
+}
+squares();
+
---
+++ week1.day4/oddnumbers.js
@@ -0,0 +1,12 @@
+for (i=0; i<=25; i++)   // for loop i should not be greater than 25 
+    
+    {
+    if (i%2!==0)        // i modulus by 2 and remainder not equal to 0
+        { 
+        console.log("The number is odd" , i)   // print the odd numbers
+    } /*else {
+console.log("The number is even" , i);
+
+    }*/
+}
+
